28d01695515bf88012c5616ab13d7ab6e3416b14
Branden <brandenk514@gmail.com>
2016-04-04 10:41:35 +0200

    Initial Commit

---
+++ Assets/Scripts/DestroyScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag ("Player")) {
+			Debug.Break ();
+			return;
+		} else {
+			Destroy (other.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnEnvironment.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnvironment : MonoBehaviour {
+
+	public GameObject plane;
+	public float zPos;
+
+	private Vector3 originPos;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		originPos = plane.transform.position;
+		zPos = plane.transform.position.z;
+		player = GameObject.FindGameObjectWithTag ("Player");
+		Spawn ();
+	}
+
+	private void Spawn() {
+		Vector3 nextPos = originPos + new Vector3 (0, plane.transform.position.y, 10 * zPos++);
+		Instantiate (plane, nextPos, Quaternion.identity);
+		Invoke ("Spawn", 0f);
+	}
+}
