6bbc9eabd42648344b7fab387981049718772aa1
Branden <brandenk514@gmail.com>
2016-04-28 12:30:44 +0200

    Fixed all prefabs and in scene colliders
    Fixed

--- Assets/Scripts/comp_cs.cs
+++ Assets/Scripts/comp_cs.cs
@@ -8,17 +8,14 @@ public class comp_cs : MonoBehaviour {
 	Tail0,Tail1,Tail2,Tail3,Tail4,Tail5,Tail6,Tail7,Tail8,Arm1,Arm2;
 	float turn,pitch,pitch2,open,balance,temp,velocity,jumpforce,animcount, Scale = 0.0F;
 	bool reset,soundplayed;
-	int lodselect=0, skinselect =0, eyeselect=0;
 	string infos;
 	Animator anim;
-	AudioSource source;
-	LODGroup lods;
-	SkinnedMeshRenderer[] rend;
 	public Texture[] skin,eyes;
 	public AudioClip Smallstep,Comp_Roar1,Comp_Roar2,Comp_Call1,Comp_Call2,Comp_Jump,Bite;
 	public float startTimer = 0f;
 	public int tripCount;
 	public bool tripped, jump,isdead = false;
+	private bool keysLocked = false;
 
 	void Awake ()
 	{
@@ -39,17 +36,15 @@ public class comp_cs : MonoBehaviour {
 		Spine5 = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5");
 		Arm1 = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/left arm0");
 		Arm2 = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/right arm0");
-		Neck0  = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/neck0");
-		Neck1  = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/neck0/neck1");
-		Neck2  = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/neck0/neck1/neck2");
-		Neck3  = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/neck0/neck1/neck2/neck3");
-		Head   = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/neck0/neck1/neck2/neck3/head");
-		Jaw    = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/neck0/neck1/neck2/neck3/head/jaw0");
+		Neck0 = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/neck0");
+		Neck1 = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/neck0/neck1");
+		Neck2 = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/neck0/neck1/neck2");
+		Neck3 = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/neck0/neck1/neck2/neck3");
+		Head = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/neck0/neck1/neck2/neck3/head");
+		Jaw = this.transform.Find ("Comp/root/spine0/spine1/spine2/spine3/spine4/spine5/neck0/neck1/neck2/neck3/head/jaw0");
 	
-		source = GetComponent<AudioSource>();
-		anim = GetComponent<Animator>();
-		lods = GetComponent<LODGroup>();
-		rend = GetComponentsInChildren <SkinnedMeshRenderer>();
+		anim = GetComponent<Animator> ();
+
 	}
 
 	void OnCollisionEnter(Collision collision )
@@ -62,6 +57,12 @@ public class comp_cs : MonoBehaviour {
 		
 	void Update ()
 	{
+
+		if (Input.GetKey (KeyCode.Space) || Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.D)) {
+			keysLocked = true;
+		} else {
+			keysLocked = false;
+		}
 		//***************************************************************************************
 		//Moves animation controller
 
@@ -88,9 +89,9 @@ public class comp_cs : MonoBehaviour {
 		else if (Input.GetKey (KeyCode.W)) anim.SetInteger ("State", 1); //Walk
 		else if (Input.GetKey (KeyCode.S)) anim.SetInteger ("State", -1); //Steps Back*/
 		if (!jump) {
-			if (Input.GetKey (KeyCode.A))
+			if (Input.GetKey (KeyCode.A) && !keysLocked)
 				anim.SetInteger ("State", 2); //Strafe+
-			else if (Input.GetKey (KeyCode.D))
+			else if (Input.GetKey (KeyCode.D)&& !keysLocked)
 				anim.SetInteger ("State", -2); //Strafe-
 		}
 		//else if (Input.GetKey (KeyCode.LeftControl)) anim.SetInteger ("State", -4); //Steps Idle
@@ -107,27 +108,7 @@ public class comp_cs : MonoBehaviour {
 				anim.SetInteger ("State", 4);
 			}
 		}
-
-		/*if (Input.GetKey (KeyCode.Alpha1))
-			anim.SetInteger ("Idle", 1); //Idle 1
-		else if (Input.GetKey (KeyCode.Alpha2))
-			anim.SetInteger ("Idle", 2); //Idle 2
-		else if (Input.GetKey (KeyCode.Alpha3))
-			anim.SetInteger ("Idle", 3); //Idle 3
-		else if (Input.GetKey (KeyCode.Alpha4))
-			anim.SetInteger ("Idle", 4); //Eat
-		else if (Input.GetKey (KeyCode.Alpha5))
-			anim.SetInteger ("Idle", 5); //Drink
-		else if (Input.GetKey (KeyCode.Alpha6))
-			anim.SetInteger ("Idle", 6); //Sleep
-		else if (Input.GetKey (KeyCode.Alpha7))
-			anim.SetInteger ("Idle", -1); //Kill
-		else
-			anim.SetInteger ("Idle", 0);
-
-		if (Input.GetKey (KeyCode.E)) anim.SetBool ("Growl", true);
-		else anim.SetBool ("Growl", false);*/
-
+			
 		//Reset spine position
 		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|SleepLoop") ||
 		    anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|Die") ||
@@ -138,73 +119,6 @@ public class comp_cs : MonoBehaviour {
 		    anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|StandEat") ||
 		    anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|GroundAttack")
 		   ) reset = true; else reset = false;
-
-
-
-
-		//***************************************************************************************
-		//Spine control
-		/*if (Input.GetKey (KeyCode.Mouse1) && reset == false)
-		{
-			turn += Input.GetAxis ("Mouse X") * 0.5F;
-			pitch += -Input.GetAxis ("Mouse Y") * 0.5F;
-			
-			if (pitch < 0.0F)
-				pitch2 += -Input.GetAxis ("Mouse Y") * 8.0F;
-			else
-				pitch2 -= Input.GetAxis ("Mouse Y") * 8.0F;
-		}
-		else if (turn != 0.0F || pitch != 0.0F || pitch2 != 0.0F)
-		{
-			if (turn < 0.0F)
-			{
-				if (turn < -0.25F)
-					turn += 0.25F;
-				else
-					turn = 0.0F; 
-			} 
-			else if (turn > 0.0F)
-			{
-				if (turn > 0.25F)
-					turn -= 0.25F;
-				else
-					turn = 0.0F; 
-			}
-			
-			if (pitch < 0.0F)
-			{
-				if (pitch < -0.25F)
-					pitch += 0.25F;
-				else {
-					pitch = 0.0F;
-					reset = false;
-				}
-			}
-			else if (pitch > 0.0F)
-			{
-				if (pitch > 0.25F)
-					pitch -= 0.25F;
-				else
-				{
-					pitch = 0.0F;
-					reset = false;
-				}
-			}
-			
-			if (pitch2 < 0.0F)
-			{
-				if (pitch2 < -1.0F)
-					pitch2 += 1.0F;
-				else
-					pitch2 = 0.0F;
-			}
-		}*/
-		
-		//Jaw control
-		if (Input.GetKey (KeyCode.E) && (
-			anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|Strafe+") ||
-			anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|Strafe-")))
-			open -= 5.0F; else open += 1.0F;
 
 
 		//Reset position
@@ -225,301 +139,6 @@ public class comp_cs : MonoBehaviour {
 					balance = 0.0F; 
 			}
 		}
-
-		/*
-		//***************************************************************************************
-		//Sound Fx code
-		
-		//Get current animation point
-		animcount = (anim.GetCurrentAnimatorStateInfo (0).normalizedTime) % 1.0F;
-		if(anim.GetAnimatorTransitionInfo(0).normalizedTime!=0.0F) animcount=0.0F;
-		animcount = Mathf.Round(animcount * 30);
-
-
-		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|Walk") ||
-		    anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|Walk-"))
-		{
-			if(soundplayed==false && animcount==11)
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Smallstep,0.05F);
-				soundplayed=true;
-			}
-			if(animcount!=11) soundplayed=false;
-		}
-		
-		
-		else if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|Strafe-") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|Strafe+"))
-		{
-			if(soundplayed==false && (animcount==11))
-			{
-				source.pitch=Random.Range(1.1F, 1.25F);
-				source.PlayOneShot(Smallstep,0.5F);
-				soundplayed=true;
-			}
-			if(animcount!=11) soundplayed=false;
-			
-		}
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|StandB") ||
-		    anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|StandB"))
-		{
-			if(soundplayed==false && animcount==2)
-			{
-				source.pitch=Random.Range(0.9F, 1.1F);
-				source.PlayOneShot(Comp_Call1,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(animcount!=2) soundplayed=false;
-		}
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|StandC") ||
-		    anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|StandC"))
-		{
-			if(soundplayed==false && animcount==2)
-			{
-				source.pitch=Random.Range(0.9F, 1.1F);
-				source.PlayOneShot(Comp_Call2,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(animcount!=2) soundplayed=false;
-		}
-
-	
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|StandD") ||
-		    anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|StandD"))
-		{
-			if(soundplayed==false && animcount==2)
-			{
-				source.pitch=Random.Range(0.9F, 1.1F);
-				source.PlayOneShot(Comp_Roar2,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(animcount!=2) soundplayed=false;
-		}
-
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|StandE") ||
-		    anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|StandE"))
-		{
-			if(soundplayed==false &&(animcount==1 || animcount==8 || animcount==16))
-			{
-				source.pitch=Random.Range(0.9F, 1.1F);
-				source.PlayOneShot(Comp_Call1,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(animcount!=1 && animcount!=8 && animcount!=16) soundplayed=false;
-		}
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|EatA") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|EatA"))
-		{
-
-			if(soundplayed==false && animcount==6)
-			{
-				source.pitch=Random.Range(1.5F, 2.0F);
-				source.PlayOneShot(Bite,Random.Range(0.1F, 0.2F));
-				source.PlayOneShot(Comp_Jump,Random.Range(0.1F, 0.3F));
-				soundplayed=true;
-			}
-			else if(animcount!=6) soundplayed=false;
-		}
-
-
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|AttackA") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|AttackA")||
-		         anim.GetNextAnimatorStateInfo (0).IsName ("Comp|AttackB") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|AttackB") ||
-		         anim.GetNextAnimatorStateInfo (0).IsName ("Comp|JumpLoopAttack") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|JumpLoopAttack"))
-		{
-			if(soundplayed==false &&(animcount==2))
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Comp_Roar1,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(soundplayed==false &&(animcount==12))
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Bite,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(animcount!=2 && animcount!=12) soundplayed=false;
-		}
-
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|RunAttackA") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|RunAttackA"))
-		{
-			if(soundplayed==false &&(animcount==2))
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Comp_Roar2,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(soundplayed==false &&(animcount==15))
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Bite,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(animcount!=2 && animcount!=15) soundplayed=false;
-		}
-
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|RunAttackB") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|RunAttackB"))
-		{
-			if(soundplayed==false &&(animcount==3))
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Comp_Roar1,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(soundplayed==false &&(animcount==15))
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Bite,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(animcount!=3 && animcount!=15) soundplayed=false;
-		}
-
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|JumpAttack") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|JumpAttack"))
-		{
-			if(soundplayed==false &&(animcount==3))
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Comp_Roar2,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(soundplayed==false &&(animcount==20))
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Bite,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(animcount!=3 && animcount!=20) soundplayed=false;
-		}
-
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|GroundAttack") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|GroundAttack"))
-		{
-			if(soundplayed==false &&(animcount==3))
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Comp_Roar1,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(soundplayed==false &&(animcount==5))
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Bite,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(animcount!=3 && animcount!=5) soundplayed=false;
-
-		}
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|Run") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|Run") ||
-		         anim.GetNextAnimatorStateInfo (0).IsName ("Comp|RunGrowl") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|RunGrowl"))
-		{
-			
-			if(soundplayed==false && animcount==4 && (
-				anim.GetNextAnimatorStateInfo (0).IsName ("Comp|RunGrowl") ||
-				anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|RunGrowl")))
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Comp_Call1,Random.Range(0.75F, 1.00F));
-				soundplayed=true;
-			} 
-	
-			if(soundplayed==false && (animcount==10 || animcount==25))
-			{
-				source.pitch=Random.Range(1.1F, 1.25F);
-				source.PlayOneShot(Smallstep,Random.Range(0.4F, 0.6F));
-				soundplayed=true;
-			}
-			else if(animcount!=4 && animcount!=10 && animcount!=25) soundplayed=false;
-		}
-
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|StandJumpUp") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|StandJumpUp") ||
-		         anim.GetNextAnimatorStateInfo (0).IsName ("Comp|RunJumpUp") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|RunJumpUp"))
-		{
-			if(soundplayed==false && animcount==4)
-			{
-				source.pitch=Random.Range(0.9F, 1.1F);
-				source.PlayOneShot(Comp_Jump,Random.Range(0.5F, 0.75F));
-				soundplayed=true;
-			}
-			else if(animcount!=4 ) soundplayed=false;
-			
-		}
-
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|StandJumpDown") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|StandJumpDown") ||
-		         anim.GetNextAnimatorStateInfo (0).IsName ("Comp|RunJumpDown") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|RunJumpDown"))
-		{
-			if(soundplayed==false && animcount==4)
-			{
-				source.pitch=Random.Range(0.75F, 1.0F);
-				source.PlayOneShot(Smallstep,Random.Range(0.4F, 0.6F));
-				soundplayed=true;
-			}
-			else if(animcount!=4 ) soundplayed=false;
-			
-		}
-
-
-		else if (!isdead && ( anim.GetNextAnimatorStateInfo (0).IsName ("Comp|Die") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|Die")))
-		{
-			if(soundplayed==false && animcount==4)
-			{
-				source.pitch=Random.Range(0.75F, 1.0F);
-				source.PlayOneShot(Comp_Roar1,Random.Range(0.5F, 1.0F));
-				soundplayed=true;
-			}
-			if(soundplayed==false && animcount==20)
-			{
-				source.PlayOneShot(Smallstep,Random.Range(0.5F, 1.0F));
-				soundplayed=true;
-			}
-			else if(animcount!=4 && animcount!=20  ) soundplayed=false;
-
-			if(animcount>20) isdead=true;
-		}
-
-
-		else if (anim.GetNextAnimatorStateInfo (0).IsName ("Comp|Rise") ||
-		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|Rise"))
-		{
-			isdead=false;
-
-			if(soundplayed==false && animcount==1)
-			{
-				source.pitch=Random.Range(1.0F, 1.25F);
-				source.PlayOneShot(Comp_Roar2,Random.Range(0.5F, 1.0F));
-				soundplayed=true;
-			}
-
-			else if(animcount!=1) soundplayed=false;
-		}
-
-		*/
 
 	}
 
@@ -610,7 +229,7 @@ void FixedUpdate ()
 		//adjust speed to the model's scale
 		Scale = this.transform.localScale.x;
 		//adjust gravity to the model's scale
-		Physics.gravity = new Vector3(0, -Scale*40.0f, 0);
+		Physics.gravity = new Vector3(0, -Scale*60.0f, 0);
 
 
 		//Walking
@@ -618,12 +237,12 @@ void FixedUpdate ()
 		    	 anim.GetNextAnimatorStateInfo (0).IsName ("Comp|Walk"))
 
 		{
-			if(Input.GetKey(KeyCode.A))
+			if(Input.GetKey(KeyCode.A) && !keysLocked)
 			{
 				transform.localRotation *= Quaternion.AngleAxis (2.0F, new Vector3 (0, -1, 0));
 				balance += 0.5F;
 			}
-			else if(Input.GetKey(KeyCode.D))
+			else if(Input.GetKey(KeyCode.D)&& !keysLocked)
 			{
 				this.transform.localRotation *= Quaternion.AngleAxis (2.0F, new Vector3 (0, 1, 0));
 				balance -= 0.5F;
@@ -646,12 +265,12 @@ void FixedUpdate ()
 			    anim.GetNextAnimatorStateInfo (0).IsName ("Comp|Walk-"))
 				
 			{
-				if(Input.GetKey(KeyCode.D))
+			if(Input.GetKey(KeyCode.D) && !keysLocked)
 				{
 					transform.localRotation *= Quaternion.AngleAxis (2.0F, new Vector3 (0, -1, 0));
 					balance += 0.5F;
 				}
-				else if(Input.GetKey(KeyCode.A))
+			else if(Input.GetKey(KeyCode.A) && !keysLocked)
 				{
 					this.transform.localRotation *= Quaternion.AngleAxis (2.0F, new Vector3 (0, 1, 0));
 					balance -= 0.5F;
@@ -683,12 +302,12 @@ void FixedUpdate ()
 		         anim.GetNextAnimatorStateInfo (0).IsName ("Comp|RunJumpDown") ||
 		         anim.GetCurrentAnimatorStateInfo (0).IsName ("Comp|RunJumpDown"))
 		{
-			if(Input.GetKey(KeyCode.A))
+			if(Input.GetKey(KeyCode.A) && !keysLocked)
 			{
 				this.transform.localRotation *= Quaternion.AngleAxis (2.0F, new Vector3 (0, -1, 0));
 				balance += 0.5F;
 			}
-			else if(Input.GetKey(KeyCode.D))
+			else if(Input.GetKey(KeyCode.D) && !keysLocked)
 			{
 				this.transform.localRotation *= Quaternion.AngleAxis (2.0F, new Vector3 (0, 1, 0));
 				balance -= 0.5F;
@@ -773,12 +392,12 @@ void FixedUpdate ()
 		{
 			anim.SetBool("Onground", false);
 
-			if(Input.GetKey(KeyCode.A))
+			if(Input.GetKey(KeyCode.A) && !keysLocked)
 			{
 				this.transform.localRotation *= Quaternion.AngleAxis (2.0F, new Vector3 (0, -1, 0));
 				balance += 0.5F;
 			}
-			else if(Input.GetKey(KeyCode.D))
+			else if(Input.GetKey(KeyCode.D) && !keysLocked)
 			{
 				this.transform.localRotation *= Quaternion.AngleAxis (2.0F, new Vector3 (0, 1, 0));
 				balance -= 0.5F;
