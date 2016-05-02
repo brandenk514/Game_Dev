af91a1ef86f1ff9fba722acdc85038ca37232a15
Branden <brandenk514@gmail.com>
2016-05-02 09:12:14 +0200

    Add power up spawning algorithm
    Fixed stuff

--- Assets/Scripts/SpawnObs.cs
+++ Assets/Scripts/SpawnObs.cs
@@ -1,30 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SpawnObs : MonoBehaviour {
 
 	//public GameObject[] spawnPoints;
-	public GameObject[] obstacles;
+	private List<GameObject> obstacles;
 
 	// Use this for initialization
 	void Start () {
 		//spawnPoints = GameObject.FindGameObjectsWithTag ("spawnPoint");
-		Spawn ();
-	}
+		obstacles = new List<GameObject>();
+		// fill obstacles array
+		for (int i = 0; i < transform.childCount; i++) {
+			if (transform.GetChild (i).tag.Equals ("Obstacle")) {
+				obstacles.Add (transform.GetChild (i).gameObject);
+				// obstacles [nextIndex] = transform.GetChild (i).gameObject;
+			//} else if (transform.GetChild (i).tag.Equals ("Pickup")){
 
-	void Spawn() {
-		for (int i = 0; i < obstacles.Length; i++) {
-			int coinFlip = Random.Range (0, 20);
-			if (coinFlip >= 17) {
-				coinFlip = Random.Range (0, 2);
-				if (coinFlip > 1) {
-					Instantiate (obstacles [i+1], transform.position, Quaternion.identity);
-				} else {
-					Instantiate (obstacles [i], transform.position, Quaternion.identity);
-				}
 			}
 		}
+		// deal with objects array
+		// fill by hand with prefabs and instantiate or activate exisiting objects?
+			
+		if (Random.value <= 0.5f)
+			this.obstacles.ToArray () [Random.Range (0, obstacles.Count)].SetActive (true);
+		//if (Random.value <= 0.4f)
+		//	SpawnPickups ();
 	}
+
+	private void SpawnPickups() {
+
+
+
+//		for (int i = 0; i < obstacles.Length; i++) {
+//			int coinFlip = Random.Range (0, 20);
+//			if (coinFlip >= 17) {
+//				coinFlip = Random.Range (0, 2);
+//				if (coinFlip > 1) {
+//					Instantiate (obstacles [i+1], transform.position, Quaternion.identity);
+//				} else {
+//					Instantiate (obstacles [i], transform.position, Quaternion.identity);
+//				}
+//			}
+//		}
+	}
+
+//	private void activateObstacles(){
+//		Debug.Log (this.obstacles.Count);
+//		this.obstacles.ToArray () [Random.Range (0, obstacles.Count)].SetActive (true);
+//	}
 }
 
 		/*Instantiate (obstacles [Random.Range (0, obstacles.Length)], transform.position, Quaternion.identity);
